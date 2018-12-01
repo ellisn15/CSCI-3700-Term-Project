277e3d18a2dc0b165708db60a94aba4329d2a089
ellisn15 <ellisn15@students.ecu.edu>
2018-12-01 14:44:30 -0500

    Create data and songs.sql

---
+++ songs.sql
@@ -0,0 +1,41 @@
+-- MySQL / SQL Server / Oracle / MS Access
+
+
+CREATE TABLE song
+(
+	sid 	integer NOT NULL,					-- song id
+	sname 	character varying(35) NOT NULL,		-- song name
+	artist 	character varying(35) NOT NULL,		-- artist name
+	
+	CONSTRAINT PK_SONG PRIMARY KEY(sid)			-- primary key made up of song id column
+);
+
+
+CREATE TABLE info
+(
+	sid 	integer NOT NULL,					-- song id
+	rank 	integer NOT NULL,					-- song rank
+	year 	integer NOT NULL,					-- song release year
+	source 	integer NOT NULL,					-- source
+
+	CONSTRAINT PK_INFO PRIMARY KEY(sid)			-- primary key made up of song id column
+);
+
+
+CREATE TABLE lyrics
+(
+	sid 	integer NOT NULL,					-- song id
+	lyrics 	text    NOT NULL,					-- song lyrics
+
+	CONSTRAINT PK_LYRICS PRIMARY KEY(sid) 		-- primary key made up of song id column
+);
+
+
+-- Load data into song table (Sid, Song, Artist)
+COPY song(Sid, Song, Artist) FROM '/Users/nickellis/Documents/Classes/CSCI_3700/Term Project/Data Files/song.csv' DELIMITER ',' CSV HEADER;
+
+-- Load data into info table (Sid, Rank, Year, Source)
+COPY info(Sid, Rank, Year, Source) FROM '/Users/nickellis/Documents/Classes/CSCI_3700/Term Project/Data Files/info.csv' DELIMITER ',' CSV HEADER;
+
+-- Load data into lyrics table (Sid, lyrics)
+COPY lyrics(Sid, Lyrics) FROM '/Users/nickellis/Documents/Classes/CSCI_3700/Term Project/Data Files/lyrics.csv' DELIMITER ',' CSV HEADER;
